3bbd04233fede69aa920999fee791bd18a5ac267
Joohwan Seo <songqkaoswe@naver.com>
2024-05-02 20:06:34 +0900

    update thumnail file

---
+++ thumnail/video.js
@@ -0,0 +1,22 @@
+// const video = {
+//   "iso_639_1": "ko",
+//       "iso_3166_1": "KR",
+//       "name": "[혹성탈출: 새로운 시대] 슈퍼볼 예고편",
+//       "key": "LU7ujHmsc-s",
+//       "site": "YouTube",
+//       "size": 1080,
+//       "type": "Trailer",
+//       "official": true,
+//       "published_at": "2024-02-13T02:00:10.000Z",
+//       "id": "65cb0bec8c31590149388749"
+// }
+// document.getElementById('youtube').src =
+//  `https://www.youtube.com/embed/` + video.key 
+
+
+//  <iframe class="youtube"
+//   style=" width: 400px; height: 400px;"
+//   src="">
+// </iframe>
+
+  
